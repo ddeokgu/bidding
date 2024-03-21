44e8576e0686bd5afe1d8313e6984847cc750b98
thecar <ddeokgu@gmail.com>
2024-03-21 16:59:51 +0900

    first commit

---
+++ src/test/java/com/thecar/bidding/BiddingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thecar.bidding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiddingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
